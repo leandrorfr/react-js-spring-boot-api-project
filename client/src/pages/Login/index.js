aa1a7ae006d932ebf1e73af2d542badc269f2acf
Leandro Ramos <leandro.ramos@totvs.com.br>
2023-01-27 13:26:21 -0300

    projeto finalizado e pronto para build

---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import { useNavigate} from 'react-router-dom';
+import './styles.css';
+
+import api from '../../services/api'
+
+import logo from '../../assets/r.png';
+
+
+export default function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function login(e){
+        e.preventDefault();
+
+        const data = {
+            username,
+            password,
+        };
+
+        try {
+            const response = await api.post('auth/signin', data);
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('accessToken', response.data.token);
+
+            navigate('/books')
+        } catch (err) {
+            navigate('/books')
+            alert('Login failed! Try again!');
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <section className="form">
+                <img src={logo} alt="Erudio Logo"/>
+                <form onSubmit={login}>
+                    <h1>Access your Account</h1>
+                    <input
+                        placeholder="Username"
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="password" placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
+                    <button className="button" type="submit">Login</button>
+                </form>
+
+            </section>
+
+        </div>
+    )
+
+}
